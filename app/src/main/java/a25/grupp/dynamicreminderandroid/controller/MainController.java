de410eeb79985ad143b6a8eb2c3d3a143ff13809
Anni Johansson <annij@live.se>
2020-04-24 11:28:21 +0200

    Ändrat i controller

--- app/src/main/java/a25/grupp/dynamicreminderandroid/controller/MainController.java
+++ app/src/main/java/a25/grupp/dynamicreminderandroid/controller/MainController.java
@@ -1,13 +1,17 @@
 package a25.grupp.dynamicreminderandroid.controller;
 
+import java.time.LocalTime;
 import java.util.Date;
 
 import a25.grupp.dynamicreminderandroid.model.FileHandler;
+import a25.grupp.dynamicreminderandroid.model.PossibleTime;
 import a25.grupp.dynamicreminderandroid.model.Task;
 import a25.grupp.dynamicreminderandroid.model.TaskRegister;
+import a25.grupp.dynamicreminderandroid.model.TimeSpan;
+import a25.grupp.dynamicreminderandroid.model.TimeUnit;
 
 /**
- * @author Hanna My Jansson
+ * @author Hanna My Jansson, Anni Johansson
  * @version 1.1
  */
 public class MainController {
@@ -21,8 +25,9 @@ public class MainController {
         viewController = new ViewController(this);
     }
 
+    // Loading saved data from file to the app
     public void loadApp() {
-        /*
+
         fileHandler = new FileHandler();
         TaskRegister loadedRegister = null;
         try {
@@ -36,92 +41,60 @@ public class MainController {
         System.out.println("Controller: filehandler created");
         loadTasksToGUI();
 
-        UpdateThread updateThread = new UpdateThread(this);
-        updateThread.start();
-
-        frame.setCard("1");
-
-         */
+        // todo Ska vi använda/göra om denna tråd?
+       // UpdateThread updateThread = new UpdateThread(this);
+        //updateThread.start();
     }
 
+    // Add a new task when clicking SAVE-button in the detailed view
+    public void addTask()
+    {
+        Task task = null;
+        int selectedTaskId = viewController.getSelectedTaskId();
 
-    public void buttonPressed() {
-        /*
-        switch (button) {
-            case TASKS:
-                frame.setCard("1");
-                break;
-            case ADD:
+        String title = viewController.getTaskTitle();
+        String info = viewController.getNotes();
 
-                frame.setDefaultFields();
-                frame.setSelectedTaskId(0);
-                frame.setCard("2");
-                break;
-            case DELETE:
-                //frame.setCard("3");
-                break;
-            case CALENDAR:
-                //frame.setCard("4");
-                break;
-            case STATISTICS:
-                //frame.setCard("5");
+        //preferredIntervall
+        int intervalAmount = 0;
+        try {
+            intervalAmount = Integer.parseInt(viewController.getIntervalAmount());
+        } catch (Exception e) {
+           //todo Här ska läggas in ett felmeddelande
 
-
-                break;
-
-            case MARKASDONE:
-
-
-                break;
-            case SAVE:
-
-                Task task = null;
-                int selectedTaskId = frame.getSelectedTaskId();
-
-                String title = frame.getTaskTitle();
-                String info = frame.getNotes();
-
-                //preferredIntervall
-                int intervalAmount = 0;
-                try {
-                    intervalAmount = Integer.parseInt(frame.getIntervalAmount());
-                } catch (Exception e) {
-                    JOptionPane.showMessageDialog(frame, "You need to add a preferred interval as a number");
-                    break;
-                }
-
-                TimeUnit timeUnit = frame.getIntervalUnit();
-                System.out.println("Controller - intervall: " + intervalAmount + " " + timeUnit);
-                TimeSpan preferredInterval = new TimeSpan(intervalAmount, timeUnit);
-
-                PossibleTime possibleTime = new PossibleTime();
-                possibleTime.setPossibleWeekDays(frame.getPossibleWeekdays());
-                possibleTime.setPossibleDates(frame.getPossibleDates());
-                LocalTime[] localTimes = frame.getPossibleHours();
-                possibleTime.setPossibleHours(localTimes[0], localTimes[1]);
-
-                if (selectedTaskId <= 0) {
-                    task = new Task(title, info, preferredInterval);
-                    task.setPossibleTimeForExecution(possibleTime);
-                    taskRegister.addTask(task);
-                    frame.addTask(task.getTitle(), task.getTimeUntil(), task.getTimeUnit(), task.getId());
-
-                } else {
-                    task = taskRegister.getTaskWithId(selectedTaskId);
-                    task.setInfo(info);
-                    task.setTitle(title);
-                    task.setPreferredInterval(preferredInterval);
-                    task.setPossibleTimeForExecution(possibleTime);
-
-                }
-                updateGUI();
-                frame.setCard("1");
-                fileHandler.saveToFile(taskRegister); //todo flytta! Just nu sparar den bara 1? tror jag
-                break;
         }
 
-         */
+        TimeUnit timeUnit = viewController.getIntervalUnit();
+        System.out.println("Controller - intervall: " + intervalAmount + " " + timeUnit);
+        TimeSpan preferredInterval = new TimeSpan(intervalAmount, timeUnit);
 
+        PossibleTime possibleTime = new PossibleTime();
+        possibleTime.setPossibleWeekDays(viewController.getPossibleWeekdays());
+        possibleTime.setPossibleDates(viewController.getPossibleDates());
+        LocalTime[] localTimes = viewController.getPossibleHours();
+        possibleTime.setPossibleHours(localTimes[0], localTimes[1]);
+
+        if (selectedTaskId <= 0) {
+            task = new Task(title, info, preferredInterval);
+            task.setPossibleTimeForExecution(possibleTime);
+            taskRegister.addTask(task);
+            viewController.addTask(task.getTitle(), task.getTimeUntil(), task.getTimeUnit(), task.getId());
+
+        } else {
+            task = taskRegister.getTaskWithId(selectedTaskId);
+            task.setInfo(info);
+            task.setTitle(title);
+            task.setPreferredInterval(preferredInterval);
+            task.setPossibleTimeForExecution(possibleTime);
+
+        }
+
+
+        //todo
+        updateGUI();
+        //viewController.setCard("1");
+
+        fileHandler.saveToFile(taskRegister); //todo flytta! Just nu sparar den bara 1? tror jag
 
     }
 
