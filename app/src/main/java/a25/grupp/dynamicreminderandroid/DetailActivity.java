8c9840bdee7b28f0159b44678b0c484e726ec843
Anni Johansson <annij@live.se>
2020-04-24 14:55:14 +0200

    DetailActivity SAVE börjat fixa

--- app/src/main/java/a25/grupp/dynamicreminderandroid/DetailActivity.java
+++ app/src/main/java/a25/grupp/dynamicreminderandroid/DetailActivity.java
@@ -1,14 +1,16 @@
 package a25.grupp.dynamicreminderandroid;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+
+import a25.grupp.dynamicreminderandroid.model.TimeUnit;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -24,7 +26,7 @@ public class DetailActivity extends AppCompatActivity {
         constraintLayout.setVisibility(View.GONE);
 
 
-        Spinner dropDownAlways = findViewById(R.id.dropDownAlways);
+        final Spinner dropDownAlways = findViewById(R.id.dropDownAlways);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.available, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropDownAlways.setAdapter(adapter);
@@ -47,7 +49,7 @@ public class DetailActivity extends AppCompatActivity {
 
             }
         });
-        Spinner dropDownTimeUnit = findViewById(R.id.dropDown_timeUnit);
+        final Spinner dropDownTimeUnit = findViewById(R.id.dropDown_timeUnit);
         ArrayAdapter<CharSequence> adapterTimeUnit = ArrayAdapter.createFromResource(this,R.array.timeunits,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropDownTimeUnit.setAdapter(adapterTimeUnit);
@@ -66,6 +68,36 @@ public class DetailActivity extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String title = findViewById(R.id.editText).toString();
+                int intervalNbr = Integer.parseInt(findViewById(R.id.editText2).toString());  //Tänker jag rätt med siffra här?
+
+                // Get the correct TimeUnit from dropdown menu.
+                TimeUnit timeUnit = null;
+                switch (dropDownTimeUnit.getSelectedItem().toString()) {
+                    case "hour":
+                        timeUnit = TimeUnit.hour;
+                        break;
+                    case "day":
+                        timeUnit = TimeUnit.day;
+                        break;
+                    case "week":
+                        timeUnit = TimeUnit.week;
+                        break;
+                    case "month":
+                        timeUnit = TimeUnit.month;
+                        break;
+                    case "year":
+                        timeUnit = TimeUnit.year;
+                        break;
+                }
+
+                String info = findViewById(R.id.editText4).toString();
+
+                switch(dropDownAlways.getSelectedItem().toString())
+                {
+
+                }
+
                 Intent save = new Intent(DetailActivity.this, MainActivity.class);
                 startActivity(save);
                 //TODO här behövs det att tasken sparas
