5a7b69f3402c9bfb48712eecb88a772968abd6b8
HannaMy <HannaMyy@gmail.com>
2020-05-05 09:58:03 +0200

    kommenterat

--- app/src/main/java/a25/grupp/dynamicreminderandroid/model/FileHandler.java
+++ app/src/main/java/a25/grupp/dynamicreminderandroid/model/FileHandler.java
@@ -5,8 +5,10 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 
 import java.io.*;
+
 /**
  * Stores the data on file
+ *
  * @author Minna Röriksson, Hanna My Jansson
  * @version 1.1
  */
@@ -15,13 +17,18 @@ public class FileHandler {
     private TaskRegister taskRegister;
     private Context context;
 
-    public FileHandler(Context context){
+    public FileHandler(Context context) {
         this.context = context;
     }
 
+    /**
+     * saves a taskregister object to a file
+     *
+     * @param taskRegister the object that is to be saved
+     */
 
-    public void saveToFile(TaskRegister taskRegister){ //Eventuellt måste en lägga till en Context också
-        try{
+    public void saveToFile(TaskRegister taskRegister) { //Eventuellt måste en lägga till en Context också
+        try {
             FileOutputStream fos = context.openFileOutput("taskRegisterFile", Context.MODE_PRIVATE);
             BufferedOutputStream bos = new BufferedOutputStream(fos);
             ObjectOutputStream oos = new ObjectOutputStream(bos);
@@ -29,24 +36,8 @@ public class FileHandler {
 
             oos.close();
             fos.close();
-           // System.out.println("FileHandler: taskregister written: " + taskRegister.toString());
-        }catch(FileNotFoundException e){
-            System.out.println("FileHandler: File not found");
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void saveToFileString(String string){ //Eventuellt måste en lägga till en Context också
-        try{
-            FileOutputStream fos = context.openFileOutput("str", Context.MODE_APPEND);
-            //BufferedOutputStream bos = new BufferedOutputStream(fos);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(string);
-            oos.close();
-            System.out.println("Oos: "+ oos + ", string: " + string);
             // System.out.println("FileHandler: taskregister written: " + taskRegister.toString());
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("FileHandler: File not found");
             e.printStackTrace();
         } catch (IOException e) {
@@ -54,50 +45,27 @@ public class FileHandler {
         }
     }
 
-
-    public TaskRegister readFromFile(){
-        try
-        {
+    /**
+     * reads a taskregister object from a file
+     *
+     * @return the taskregister from the file
+     */
+    public TaskRegister readFromFile() {
+        try {
             FileInputStream fis = context.openFileInput("taskRegisterFile");
             //BufferedInputStream bis = new BufferedInputStream(fis);
             ObjectInputStream ois = new ObjectInputStream(fis);
-            taskRegister =  (TaskRegister)ois.readObject();
+            taskRegister = (TaskRegister) ois.readObject();
             System.out.println("FileHandler: Task read: " + taskRegister.toString());
             fis.close();
             ois.close();
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("FileHandler: File not found");
             e.printStackTrace();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         return taskRegister;
-    }
-
-
-
-    public String readFromFileString(){
-        System.out.println("read from file");
-        String string = "";
-        try
-        {
-            FileInputStream fis = context.openFileInput("str");
-            System.out.println("fis: " + fis);
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            System.out.println("ois: " + ois);
-            string =  (String)ois.readObject();
-            System.out.println("ois: " + ois + " , string: " + string);
-            //System.out.println("FileHandler: Task read: " + taskRegister.toString());
-            fis.close();
-            ois.close();
-        }catch(FileNotFoundException e){
-            System.out.println("FileHandler: File not found");
-            e.printStackTrace();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return string;
     }
 
 }
