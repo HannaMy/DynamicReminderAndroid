8a9f94d723f57e00725f18b7cd04660ad38359fe
HannaMy <HannaMyy@gmail.com>
2020-05-07 11:07:26 +0200

    change text and color of days left

--- app/src/main/java/a25/grupp/dynamicreminderandroid/AdapterTaskOverview.java
+++ app/src/main/java/a25/grupp/dynamicreminderandroid/AdapterTaskOverview.java
@@ -3,6 +3,7 @@ package a25.grupp.dynamicreminderandroid;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -17,6 +18,7 @@ import a25.grupp.dynamicreminderandroid.model.Task;
 
 /**
  * Adapts the data in to tle list view in the overview in mainActivity
+ *
  * @author Hanna My Jansson
  * @version 1.0
  */
@@ -30,25 +32,28 @@ public class AdapterTaskOverview extends ArrayAdapter {
     private int[] taskIds;
     private Activity mainActivity;
     private Task[] taskArray;
+    private String[] lateOrLeft;
 
     /**
      * Constructor
+     *
      * @param context the context from where the constructor is called
-     * @param titles teh titles of the tasks
+     * @param titles  teh titles of the tasks
      */
     public AdapterTaskOverview(@NonNull Context context, String[] titles) {
-        super(context, R.layout.task_list_item,R.id.tvTitle, titles);
+        super(context, R.layout.task_list_item, R.id.tvTitle, titles);
         System.out.println("adaper: construct");
-        this.mainActivity =(MainActivity) context;
+        this.mainActivity = (MainActivity) context;
     }
 
     /**
      * Updates the list with data
-     * @param titles of the tasks
+     *
+     * @param titles       of the tasks
      * @param intervalInfo of the tasks
-     * @param times of the tasks
-     * @param timeUnits of the tasks
-     * @param taskIds of the tasks
+     * @param times        of the tasks
+     * @param timeUnits    of the tasks
+     * @param taskIds      of the tasks
      */
     public void updateListData(String[] titles, String[] intervalInfo, int[] times, String[] timeUnits, int[] taskIds) {
         System.out.println("Adapter: update");
@@ -59,8 +64,37 @@ public class AdapterTaskOverview extends ArrayAdapter {
         this.timeUnits = timeUnits;
     }
 
+    private String lateOrLeft(Task task) {
+        int timeUntil = task.getTimeUntil();
+        String leftOrLate = "";
+        if (timeUntil > 1) {
+            leftOrLate = "s left";
+        }else if(timeUntil == 1){
+            leftOrLate = " left";
+        }else if(timeUntil == 0){
+            leftOrLate = " left";
+        }else if(timeUntil == -1){
+            leftOrLate = " late";
+        }else {
+            leftOrLate = "s late";
+        }
+        return leftOrLate;
+    }
+
+    private int textColor(Task task){
+        int timeUntil = task.getTimeUntil();
+        int color;
+        if (timeUntil >= 0) {
+            color = 0x99009900;
+        }else {
+            color = 0x99990000;
+        }
+        return color;
+    }
+
     /**
      * Sets the array with tasks
+     *
      * @param taskArray the array with tasks
      */
     public void setTaskArray(Task[] taskArray) {
@@ -69,9 +103,10 @@ public class AdapterTaskOverview extends ArrayAdapter {
 
     /**
      * Generates a view item for the list
-     * @param position of the view
+     *
+     * @param position    of the view
      * @param convertView not used
-     * @param parent not used
+     * @param parent      not used
      * @return the view generated
      */
     @NonNull
@@ -80,7 +115,7 @@ public class AdapterTaskOverview extends ArrayAdapter {
 
 
         LayoutInflater layoutInflater = (LayoutInflater) mainActivity.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View taskListItem = layoutInflater.inflate(R.layout.task_list_item,parent, false);
+        View taskListItem = layoutInflater.inflate(R.layout.task_list_item, parent, false);
 
         Button btnDetails = taskListItem.findViewById(R.id.btnExpand);
         TextView tvAmountTime = taskListItem.findViewById(R.id.tvNbrTimeLeft);
@@ -88,44 +123,47 @@ public class AdapterTaskOverview extends ArrayAdapter {
         TextView tvTitle = taskListItem.findViewById(R.id.tvTitle);
         TextView tvInterval = taskListItem.findViewById(R.id.tvInterval);
 
-       if(taskArray != null){
+        if (taskArray != null) {
 
-           tvAmountTime.setText(String.valueOf(taskArray[position].getTimeUntil()));
-           tvInterval.setText(taskArray[position].getPreferredInterval().toString());
-           tvTitle.setText(taskArray[position].getTitle());
-           tvTimeUnit.setText(taskArray[position].getPreferredInterval().getTimeUnit().toString());
+            tvAmountTime.setText(String.valueOf(taskArray[position].getTimeUntil()));
+            tvInterval.setText(taskArray[position].getPreferredInterval().toString());
+            tvTitle.setText(taskArray[position].getTitle());
 
-           btnDetails.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   Intent detail = new Intent(mainActivity, DetailActivity.class);
-                   detail.putExtra("taskId", taskArray[position].getId());
-                   System.out.println("put extra taskid " + taskArray[position].getId());
-                   mainActivity.startActivity(detail);
-               }
-           });
+            //set the time unit
+            String leftOrLate  = lateOrLeft(taskArray[position]);
+            int color = textColor(taskArray[position]);
+            tvTimeUnit.setText(taskArray[position].getPreferredInterval().getTimeUnit().toString() + leftOrLate);
+            tvAmountTime.setTextColor(color);
+            tvTimeUnit.setTextColor(color);
 
-
-       }else{
-           System.out.println("Adapter: getView " + position);
-           tvAmountTime.setText(String.valueOf(times[position]));
-           tvInterval.setText(intervalInfos[position]);
-           tvTitle.setText(titles[position]);
-           tvTimeUnit.setText(timeUnits[position]);
-
-           btnDetails.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   Intent detail = new Intent(mainActivity, DetailActivity.class);
-                   detail.putExtra("taskId", taskIds[position]);
-                   mainActivity.startActivity(detail);
-               }
-           });
-
-       }
+            btnDetails.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent detail = new Intent(mainActivity, DetailActivity.class);
+                    detail.putExtra("taskId", taskArray[position].getId());
+                    System.out.println("put extra taskid " + taskArray[position].getId());
+                    mainActivity.startActivity(detail);
+                }
+            });
 
 
+        } else {
+            System.out.println("Adapter: getView " + position);
+            tvAmountTime.setText(String.valueOf(times[position]));
+            tvInterval.setText(intervalInfos[position]);
+            tvTitle.setText(titles[position]);
+            tvTimeUnit.setText(timeUnits[position]);
 
+            btnDetails.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent detail = new Intent(mainActivity, DetailActivity.class);
+                    detail.putExtra("taskId", taskIds[position]);
+                    mainActivity.startActivity(detail);
+                }
+            });
+
+        }
         return taskListItem;
     }
 }
