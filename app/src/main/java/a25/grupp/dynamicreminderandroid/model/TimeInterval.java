624d0f4b33b7e0e06cc1374eec45461d80462de0
Cornelia <cornelia.skold@gmail.com>
2020-06-05 11:38:39 +0200

    korrekturläst och kommenterat

--- app/src/main/java/a25/grupp/dynamicreminderandroid/model/TimeInterval.java
+++ app/src/main/java/a25/grupp/dynamicreminderandroid/model/TimeInterval.java
@@ -1,7 +1,6 @@
 package a25.grupp.dynamicreminderandroid.model;
 
 import java.io.Serializable;
-import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -13,26 +12,8 @@ import java.util.Date;
 public class TimeInterval implements Serializable {
     private int from;
     private int to;
-    private boolean isEmpty;
-
-
-    public TimeInterval(int from, int to) {
-        this.from = from;
-        this.to = to;
-        isEmpty = false;
-    }
 
     public TimeInterval() {
-        isEmpty = true;
-    }
-
-
-    /**
-     * Checks if the interval is empty
-     * @return true if ther is no specified time in the interval
-     */
-    public boolean isEmpty() {
-        return isEmpty;
     }
 
 
@@ -40,26 +21,28 @@ public class TimeInterval implements Serializable {
      * Changes the interval to the given parameters from och to.
      * @param from the beginning of the interval
      * @param to the end of the interval
-     * @return true if it was able to change the interval
      */
-    public boolean setInterval(int from, int to) {
-
+    public void setInterval(int from, int to) {
         if (from<to) {
             this.from = from;
             this.to = to;
-            isEmpty = false;
-            return true;
         } else {
             System.out.println("TimeInterval rad51: false" );
-            return false;
         }
-
     }
 
+    /**
+     * Get method for from variable
+     * @return hours from
+     */
     public int getFrom() {
         return from;
     }
 
+    /**
+     * Get method for to variable
+     * @return hours to
+     */
     public int getTo() {
         return to;
     }
@@ -74,23 +57,19 @@ public class TimeInterval implements Serializable {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         int hour = cal.get(Calendar.HOUR_OF_DAY);
-        //int minute = cal.get(Calendar.MINUTE);
-
-
-
-        if (from<=hour && to>hour) {
-            return true;
-        }
-        return false;
+        return from <= hour && to > hour;
     }
 
+    /**
+     * Calculate how many hours until the chosen interval
+     *
+     * @param date the current date where the interval is added to
+     * @return the time until in hours
+     */
     public int hoursUntilInterval(Date date) {
-
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         int hour = cal.get(Calendar.HOUR);
-        //int minute = cal.get(Calendar.MINUTE);
-
         int hoursUntil = from-hour;
         if(hoursUntil<0){
             hoursUntil += 24;
@@ -98,19 +77,21 @@ public class TimeInterval implements Serializable {
         return hoursUntil;
     }
 
+    /**
+     * Calculate how many hours since last possible time for execution
+     *
+     * @param date the date for the last possible time for execution
+     * @return the time since last possible time for execution in hours
+     */
     public int hoursBeforeInterval(Date date) {
 
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         int hour = cal.get(Calendar.HOUR);
-        //int minute = cal.get(Calendar.MINUTE);
-
         int hoursBefore = hour-(to-1);
         if(hoursBefore<0){
             hoursBefore += 24;
         }
         return hoursBefore;
     }
-
-
 }
